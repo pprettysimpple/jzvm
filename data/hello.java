46318ccc6c782b77f0c1678ae21dba78dcbe2f17
Kirill Andriianov <kirill@andriianov.ru>
2024-10-14 08:32:02 +0200

    some data for manual testing

---
+++ data/hello.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    static int x = 10;
+    public static void main(String[] args) {
+	int a = 40;
+	int b = 2;
+	long c = a + b;
+	double d = (double)c;
+    }
+}
---
+++ data/welcome.java
@@ -0,0 +1,5 @@
+class Welcomer {
+    static int x = 10;
+    public static void mainnnnnn(String[] args) {
+    }
+}
